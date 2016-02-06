5fb38e4ae8ce64632b10ad71e85c5515b0ee9d4a
kperkins6 <kperkins6@mail.csuchico.edu>
2016-02-06 13:47:36 -0800

    Edited the models to reflect database relations

--- radioStar/app/models/user.rb
+++ radioStar/app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   belongs_to :station
+  has_many :shows
+  has_many :user_shows, dependent: :destroy
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
