1182d8141fe8ba4fc0c2ca55fedad8a7a9e6fa59
Sigf <seb.hutt@gmail.com>
2016-02-06 15:03:36 -0800

    making profile work with json

--- radioStar/app/assets/javascripts/profile.js
+++ radioStar/app/assets/javascripts/profile.js
@@ -1,5 +1,9 @@
 var myApp = angular.module('myApp', []);
 
+var dev_prefix = "http://localhost:3000";
+var prod_prefix = "/radiostar";
+var current_prefix = dev_prefix;
+
 myApp.config([
   "$httpProvider", function($httpProvider) {
     $httpProvider.defaults.headers.common['X-CSRF-Token'] = angular.element('meta[name=csrf-token]').attr('content');
@@ -8,16 +12,22 @@ myApp.config([
 
 myApp.controller('MyController', ['$scope', '$http', function MyController($scope, $http){
 
-  $scope.getPlaylist = function(user_id){
+  $scope.getPlaylist = function(){
     $http.get().success(function(data){
       console.log(data);
 		});
   }
 
-  $scope.getUser = function(user_id){
-    $http.get().success(function(data){
+  $scope.getShows = function(){
+    $http.get(current_prefix + '/users/' + current_user  + '/shows.json').success(function(data){
       console.log(data);
-    });
+    })
   }
+
+  $scope.init = function(){
+    $scope.getShows();
+  }
+
+    $scope.init();
 
 }]);
--- radioStar/app/controllers/profile_controller.rb
+++ radioStar/app/controllers/profile_controller.rb
@@ -3,10 +3,10 @@ class ProfileController < ApplicationController
   respond_to :json
 
   def index
-    
+
   end
 
   def show
-
+    @user = current_user
   end
 end
