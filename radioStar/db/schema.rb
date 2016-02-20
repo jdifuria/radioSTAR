ad8860f4a88e0cb0820cdc8b2922820c50f8415c
jdifuria <jaddifuria@gmail.com>
2016-02-20 02:33:30 -0800

    Added migration, user now has admin:boolean field

--- radioStar/db/schema.rb
+++ radioStar/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160220073710) do
+ActiveRecord::Schema.define(version: 20160220102906) do
 
   create_table "charts", force: :cascade do |t|
     t.string   "name"
@@ -98,6 +98,7 @@ ActiveRecord::Schema.define(version: 20160220073710) do
     t.float    "compliance"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
